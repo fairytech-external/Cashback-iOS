b05cc05660ed02195a3d909a1f6bf24c7825ab56
davidjkjk <130335901+davidjkjk@users.noreply.github.com>
2024-11-06 15:52:10 +0900

    0.8.6 alpha

--- Moment.xcframework/ios-arm64_x86_64-simulator/Moment.framework/Headers/Moment-Swift.h
+++ Moment.xcframework/ios-arm64_x86_64-simulator/Moment.framework/Headers/Moment-Swift.h
@@ -305,6 +305,8 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 SWIFT_CLASS("_TtC6Moment22CashbackViewController")
 @interface CashbackViewController : UIViewController
 - (void)viewDidLoad;
+- (void)viewWillLayoutSubviews;
+- (void)viewWillDisappear:(BOOL)animated;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
@@ -625,6 +627,8 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 SWIFT_CLASS("_TtC6Moment22CashbackViewController")
 @interface CashbackViewController : UIViewController
 - (void)viewDidLoad;
+- (void)viewWillLayoutSubviews;
+- (void)viewWillDisappear:(BOOL)animated;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil OBJC_DESIGNATED_INITIALIZER;
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
 @end
